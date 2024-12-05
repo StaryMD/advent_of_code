61e7c18f41b0c16b4c573ff4bdd939d6c9426cf7
mironicavasile <mironicavasile@gmail.com>
2024-12-05 07:20:17 +0200

    2024 5 solved

---
+++ 2023/7a.py
@@ -0,0 +1,63 @@
+from aocd import data, submit
+from numpy import prod
+
+
+def GetAns(text):
+    card_power = {
+        "A": 12,
+        "K": 11,
+        "Q": 10,
+        "J": 9,
+        "T": 8,
+        "9": 7,
+        "8": 6,
+        "7": 5,
+        "6": 4,
+        "5": 3,
+        "4": 2,
+        "3": 1,
+        "2": 0,
+    }
+
+    def Power(hand: str):
+        map = {i: hand.count(i) for i in set(hand)}
+
+        score = 0
+        for i, c in enumerate(reversed(hand)):
+            score += card_power[c] * 12**i
+
+        if len(map) == 1:
+            return 5 * 12**5 + score
+        elif len(map) == 2:
+            return 4 * 12**5 + score
+        elif 3 in [f[1] for f in map.items()]:
+            return 3 * 12**5 + score
+        elif len(map) == 3:
+            return 2 * 12**5 + score
+        elif len(map) == 4:
+            return 1 * 12**5 + score
+        return score
+
+    stuff = [line.split(" ") for line in text]
+
+    stuff = sorted(stuff, key=lambda x: Power(x[0]))
+
+    return sum((i + 1) * int(a[1]) for i, a in enumerate(stuff))
+
+
+test_input = [
+    "32T3K 765",
+    "T55J5 684",
+    "KK677 28",
+    "KTJJT 220",
+    "QQQJA 483",
+]
+
+test_points = GetAns(test_input)
+if test_points != 6440:
+    raise RuntimeError(f"Example test not passed with ans = {test_points}")
+
+points = GetAns(data.split("\n"))
+print(points)
+
+submit(points)
---
+++ 2024/5b.py
@@ -0,0 +1,83 @@
+from aocd import data, submit
+import json
+
+
+def GetAns(text):
+    br = 0
+    while text[br] != "":
+        br += 1
+
+    map = {}
+
+    for line in text[:br]:
+        a, b = [int(x) for x in line.split("|")]
+
+        if a not in map:
+            map[a] = [b]
+        else:
+            map[a].append(b)
+
+    print(json.dumps(map))
+    points = 0
+
+    for line in text[br + 1 :]:
+        nums = [int(x) for x in line.split(",")]
+
+        good = True
+        for i, num in enumerate(nums):
+            for j, num2 in enumerate(nums[i + 1 :]):
+                if num2 in map and num in map[num2]:
+                    good = False
+
+        if not good:
+            print(nums)
+            for i, num in enumerate(nums):
+                for j, num2 in enumerate(nums[i + 1 :]):
+                    if num2 in map and num in map[num2]:
+                        nums[i], nums[j + i + 1] = nums[j + i + 1], nums[i]
+                        num = nums[i]
+
+            points += nums[len(nums) // 2]
+
+    return points
+
+
+test_input = [
+    "47|53",
+    "97|13",
+    "97|61",
+    "97|47",
+    "75|29",
+    "61|13",
+    "75|53",
+    "29|13",
+    "97|29",
+    "53|29",
+    "61|53",
+    "97|53",
+    "61|29",
+    "47|13",
+    "75|47",
+    "97|75",
+    "47|61",
+    "75|61",
+    "47|29",
+    "75|13",
+    "53|13",
+    "",
+    "75,47,61,53,29",
+    "97,61,53,29,13",
+    "75,29,13",
+    "75,97,47,61,53",
+    "61,13,29",
+    "97,13,75,29,47",
+]
+
+test_points = GetAns(test_input)
+if test_points != 123:
+    raise RuntimeError(f"Example test not passed with ans = {test_points}")
+
+points = GetAns(data.split("\n"))
+print(points)
+
+submit(points)
